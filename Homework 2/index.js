1630a0b0bb74992d63d7af1cae5127b4d16af1ca
maxsozo <sozanskii.maksim@gmail.com>
2024-02-06 01:48:40 +0200

    Added fifth exercise

--- Homework 2/index.js
+++ Homework 2/index.js
@@ -1,23 +1,30 @@
 
-var a = 5;  // спочатку створюємо змінну а і передаємо значення в неї
-var b, c;   // створюємо змінні b i c
+// var a = 5;  // спочатку створюємо змінну а і передаємо значення в неї
+// var b, c;   // створюємо змінні b i c
 
-b = (a * 5);    // передаємо в змінну b значення
-b = (c = (b/2));    // передаємо в змінну b нові значення після того як передадуться значення в змінну с
-
-
-let age = +prompt("Вкажіть ваш вік");
-let yearNow = 2024;
-let dateOfBirth = (yearNow - age);
-alert(dateOfBirth);
+// b = (a * 5);    // передаємо в змінну b значення
+// b = (c = (b/2));    // передаємо в змінну b нові значення після того як передадуться значення в змінну с
 
 
-let celsius = +prompt("Вкажіть температуру в цельсіях");
-let fahrenheit = ((celsius * 9) / 5) + 32;
-alert(fahrenheit);
+// let age = +prompt("Вкажіть ваш вік");
+// let yearNow = 2024;
+// let dateOfBirth = (yearNow - age);
+// alert(dateOfBirth);
 
 
-let num1 = +prompt("Напишіть число яке хочите поділити");
-let num2 = +prompt("Вкажіть на скільки хочите поділити");
-let division = Math.floor(num1 / num2);
+// let celsius = +prompt("Вкажіть температуру в цельсіях");
+// let fahrenheit = ((celsius * 9) / 5) + 32;
+// alert(fahrenheit);
+
+
+// let num1 = +prompt("Напишіть число яке хочите поділити");
+// let num2 = +prompt("Вкажіть на скільки хочите поділити");
+// let division = Math.floor(num1 / num2);
+// alert(division);
+
+
+const eur = 41;
+let grn = +prompt("Скільки ви хочите обміняти з гивні на євро?");
+let division = grn / eur;
+division = division.toFixed(2);
 alert(division);
