3e35457be076140cfc0189bdcccb6e1f7f0ff402
maxsozo <sozanskii.maksim@gmail.com>
2024-02-10 18:23:11 +0200

    Added second exercise

--- Homework 3/index.js
+++ Homework 3/index.js
@@ -1,7 +1,12 @@
 
+// {
+//     let userName = prompt("Введіть своє ім'я");
+//     alert(`Привіт ${userName}!!`);
+// }
+
+
 {
-let userName = prompt("Введіть своє ім'я");
-alert(`Привіт ${userName}!!`);
+    let text = prompt("Введіть текст, використовуйте коми");
+    text = text.split(',').join('блін,');
+    alert(text)
 }
-
-
