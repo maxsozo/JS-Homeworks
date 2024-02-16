5978620d0400d911086817ac13b2e492da5d8a2a
maxsozo <sozanskii.maksim@gmail.com>
2024-02-16 18:57:21 +0200

    Added exercise Item access

--- Homework 5/index.js
+++ Homework 5/index.js
@@ -3,3 +3,17 @@
     let answersArr = [confirm('Ви любите пиво?'), confirm('Займаєтесь спортом?'), confirm('Ви ходите в церкву?')];
     console.log(answersArr);    
 }
+
+{
+    let answersArr = [];
+    answersArr[0] = prompt('Як вас звати?');
+    answersArr[1] = prompt('Введіть свою дату народження');
+    answersArr[2] = prompt('Ви любите пиво?');
+    console.log(answersArr);    
+}
+
+{
+    let answersArr = [prompt('У вас є машина?'),];
+    console.log(answersArr[0]);
+    console.log(answersArr.length);
+}
