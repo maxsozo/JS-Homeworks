cd37df2099412d3ce7b585eea62feecdfc142d72
maxsozo <sozanskii.maksim@gmail.com>
2024-02-19 18:38:17 +0200

    Added exercise Reduce HTML

--- Homework 5/index.js
+++ Homework 5/index.js
@@ -280,3 +280,11 @@
     let result = censorWords(userText, badWords);
     console.log(result);
 }
+
+{
+    const currencies = ["USD", "EUR", "GBP", "UAH"];
+    let str = "<select>";
+    str += currencies.reduce((a, b) => `${a}<option>${b}</option>`, "");
+    str += "</select>";
+    document.write(str);
+}
