5e37697539e4461fdd26f3bb023b556bec7f0173
maxsozo <sozanskii.maksim@gmail.com>
2024-02-19 02:05:38 +0200

    Added exercise For Table Letters

--- Homework 5/index.js
+++ Homework 5/index.js
@@ -179,3 +179,19 @@
     str+= "</table>"
     document.write(str)
 }
+
+{
+    const currencies = ["USD", "EUR", "GBP", "UAH"];
+    let str = "<table>";
+
+    for (const currency of currencies) {
+        str += "<tr>";
+        for (const letter of currency) {
+            str += "<td>" + letter + "</td>";
+        }
+        str += "</tr>";
+    }
+
+    str += "</table>";
+    document.write(str);
+}
