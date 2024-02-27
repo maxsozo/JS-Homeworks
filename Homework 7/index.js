2904c4eac493141e7d5554039d469b9af7257d93
maxsozo <sozanskii.maksim@gmail.com>
2024-02-28 00:28:42 +0200

    Added exercise Flats

--- Homework 7/index.js
+++ Homework 7/index.js
@@ -7,9 +7,18 @@
 }
 
 {
-    function RGB_background(red, green, blue){
+    function RGB_background(red, green, blue) {
         let element = document.getElementById("bg"); 
        return element.style.backgroundColor = "rgb(" + red + ", " + green + ", " + blue + ")";
     }
     RGB_background(50, 12, 100);
+}
+
+{
+    function apartmentLocation(floors, apartmentOnFloor, numApartment) {
+        let numEntrance = Math.ceil( numApartment / (floors * apartmentOnFloor));
+    let numFloor = Math.ceil(( numApartment % (floors * apartmentOnFloor)) / apartmentOnFloor);
+    return ('Квартира ' + numApartment + ' знаходится в ' + numEntrance + "-му під'їзді на " + numFloor + '-му поверсі');
+    }
+    console.log(apartmentLocation(4, 4, 81));
 }
