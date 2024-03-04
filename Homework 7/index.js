ed0662683590a6602ddeb048d646511ff3a001bf
maxsozo <sozanskii.maksim@gmail.com>
2024-03-04 17:28:07 +0200

    Added exercise Calc Func

--- Homework 7/index.js
+++ Homework 7/index.js
@@ -220,3 +220,23 @@
 ]
     tableSort(persons1, "name" , false)
 }
+
+{
+
+    function bodyWeightCheck (weight = +prompt('Введіть свою вагу'+' '+'(кг)') , heightInSm = +prompt('Введіть свій зріст'+' '+'(см)')) {
+        let heightInM = heightInSm / 100;
+        let sum = weight / (heightInM * heightInM);
+
+        if (sum <= 18.5){
+        return alert ('Давай набирай вес дрищ!');
+        }
+        else if (sum <= 25) {
+        return alert ('Красавчик!');
+        }
+        else {
+            return alert ('Роби кардіо жирна жопа!')
+        }
+    }
+
+    bodyWeightCheck()
+}
