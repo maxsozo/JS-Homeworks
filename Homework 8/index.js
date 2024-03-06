2ddb9e17e36274e0861bf625d3f95296b33f67b1
maxsozo <sozanskii.maksim@gmail.com>
2024-03-06 23:15:33 +0200

    Added exercise noswitch

--- Homework 8/index.js
+++ Homework 8/index.js
@@ -125,3 +125,27 @@ console.log(a);
         document.write("<div style='background-color: gray;'>Я не зрозумів</div>");
     }
 }
+
+{
+    const noSwitch = (key, cases, defaultKey='default') => {
+        const choseFunc = cases[key] || cases[defaultKey];
+        return choseFunc();
+    }
+    
+    
+    
+    const drink = prompt("Що ви любите пити");
+    noSwitch(drink, {
+        воду: () => console.log('Найздоровіший вибір!'),
+        чай(){
+            console.log('Смачна та корисна штука. Не перестарайтеся з цукром')
+        },
+        "пиво": () => console.log('Добре влітку, та в міру'),
+        віскі: function(){
+            console.log('Та ви, батечку, естет! Не забудьте лід і сигару')
+        },
+        default(){
+            console.log('шото я не зрозумів')
+        }
+    })
+}
