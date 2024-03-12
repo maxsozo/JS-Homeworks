476a35a0488b1520d3feb626d67ce3d329425cc3
maxsozo <sozanskii.maksim@gmail.com>
2024-03-12 18:53:01 +0200

    Added exercise array fill

--- Homework 9/index.js
+++ Homework 9/index.js
@@ -1,7 +1,18 @@
 {
     let question;
+    
     while(question = !confirm('Ви любите пиво?')) {
            
     }
     alert('Я також)');
+}
+
+{
+    let arr = [];
+    let userInput;
+
+    while(userInput = prompt('Що ви хочите поставити у вашу скриньку?')) {
+        arr.push(userInput);
+    }
+    console.log(arr);
 }
