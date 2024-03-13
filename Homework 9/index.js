0e9411d5b7c65e3c1bf147321c4016ebe518e8b9
maxsozo <sozanskii.maksim@gmail.com>
2024-03-14 01:03:34 +0200

    Added exercise multiply table

--- Homework 9/index.js
+++ Homework 9/index.js
@@ -120,3 +120,14 @@
     }
     console.log(progression);
 }
+
+{
+    let arr = [];
+    for(let i = 0; i < 5; i++) {
+        arr[i] = []; 
+        for(let j = 0; j < 5; j++) {
+            arr[i].push(i * j);
+        }
+    }
+    console.log(arr);
+}
