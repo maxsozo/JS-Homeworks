dc5966eae24f4e1c193a0f29c23a53d0342338c8
maxsozo <sozanskii.maksim@gmail.com>
2024-03-21 01:21:56 +0200

    Added exercise personForm

--- Homework 10/index.js
+++ Homework 10/index.js
@@ -1,30 +1,205 @@
-{
-    function createPerson(name, surname) {
-        return {
-            name: name,
-            surname: surname,
-            getFullName: function() {
-                if (this.fatherName) {
-                    return `${this.name} ${this.fatherName} ${this.surname}`;
-                } else {
-                    return `${this.name} ${this.surname}`;
-                }
-            }
-        }
-    }
+// {
+//     function createPerson(name, surname) {
+//         return {
+//             name: name,
+//             surname: surname,
+//             getFullName: function() {
+//                 if (this.fatherName) {
+//                     return `${this.name} ${this.fatherName} ${this.surname}`;
+//                 } else {
+//                     return `${this.name} ${this.surname}`;
+//                 }
+//             }
+//         }
+//     }
 
-    const a = createPerson("Вася", "Пупкін")
-    const b = createPerson("Ганна", "Іванова")
-    const c = createPerson("Єлизавета", "Петрова")
+//     const a = createPerson("Вася", "Пупкін")
+//     const b = createPerson("Ганна", "Іванова")
+//     const c = createPerson("Єлизавета", "Петрова")
 
-    console.log(a.getFullName()) //Вася Пупкін
-    a.fatherName = 'Іванович'    
-    console.log(a.getFullName()) //Вася Іванович Пупкін
+//     console.log(a.getFullName()) //Вася Пупкін
+//     a.fatherName = 'Іванович'    
+//     console.log(a.getFullName()) //Вася Іванович Пупкін
 
-    console.log(b.getFullName()) //Ганна Іванова
-}
+//     console.log(b.getFullName()) //Ганна Іванова
+// }
 
-{
+// {
+//     function createPersonClosure (name, surname) {
+        
+//         let fullName = name + ' ' + surname;
+//         let [firstName, lastName] = fullName.split(' ');
+//         let fatherName = '';
+//         let age = 0;
+
+//         return {
+//             getName: function () {
+//                 return name;
+//             },
+
+//             getSurname: function () {
+//                 return surname;
+//             },
+
+//             getFatherName: function () {
+//                 return fatherName;
+//             },
+
+//             getAge: function () {
+//                 return age;
+//             },
+
+//             getFullName: function () {
+//                 if (fatherName) {
+//                     return `${name} ${fatherName} ${surname}`;
+//                 } else {
+//                     return `${name} ${surname}`;
+//                 }   
+//             },
+
+//             setName: function (newName) {
+//                 name = newName;
+//             },
+
+//             setSurname: function (newSurname) {
+//                 surname = newSurname;
+//             },
+
+//             setFatherName: function (newFatherName) {
+//                 fatherName = newFatherName;
+//             },
+
+//             setAge: function (newAge) {
+//                 if (typeof newAge === 'number' && newAge >= 0 && newAge <= 100) {
+//                     age = newAge;
+//                 }
+//             },
+
+//             setFullName: function(fullName) {
+//                 const parts = fullName.split(' ');
+//                 const length = parts.length;
+//                 if (length >= 1) {
+//                     surname = parts[length - 3] || 'Анонімович';
+//                 }
+//                 if (length >= 2) {
+//                     name = parts.slice(0, length - 2).join(' ') || 'Анонім';
+//                 }
+//                 if (length >= 3) {
+//                     fatherName = parts[length - 1] || '';
+//                 }
+//             }
+//         }
+        
+//     }
+//     const a = createPersonClosure("Вася", "Пупкін")
+//     const b = createPersonClosure("Ганна", "Іванова") 
+//     console.log(a.getName())
+//     a.setAge(15)
+//     a.setAge(150) //не працює
+//     b.setFullName("Петрова Ганна Миколаївна")
+//     console.log(b.getFatherName()) //Миколаївна
+// }
+
+// {
+//         function createPersonClosureDestruct (userInfo) {
+            
+//             let {name = 'Анонім', surname = 'Анонімус', fatherName = 'Анонімович', age = 1} = userInfo;
+    
+//             return {
+//                 getName: function () {
+//                     return name;
+//                 },
+    
+//                 getSurname: function () {
+//                     return surname;
+//                 },
+    
+//                 getFatherName: function () {
+//                     return fatherName;
+//                 },
+    
+//                 getAge: function () {
+//                     return age;
+//                 },
+    
+//                 getFullName: function () {
+//                     if (fatherName) {
+//                         return `${name} ${fatherName} ${surname}`;
+//                     } else {
+//                         return `${name} ${surname}`;
+//                     }   
+//                 },
+    
+//                 setName: function (newName) {
+//                     name = newName;
+//                 },
+    
+//                 setSurname: function (newSurname) {
+//                     surname = newSurname;
+//                 },
+    
+//                 setFatherName: function (newFatherName) {
+//                     fatherName = newFatherName;
+//                 },
+    
+//                 setAge: function (newAge) {
+//                     if (typeof newAge === 'number' && newAge >= 0 && newAge <= 100) {
+//                         age = newAge;
+//                     }
+//                 },
+    
+//                 setFullName: function(fullName) {
+//                     const parts = fullName.split(' ');
+//                     const length = parts.length;
+//                     if (length >= 1) {
+//                         surname = parts[length - 3];
+//                     }
+//                     if (length >= 2) {
+//                         name = parts.slice(0, length - 2).join(' ');
+//                     }
+//                     if (length >= 3) {
+//                         fatherName = parts[length - 1];
+//                     }
+//                 }
+//             }
+            
+//         }
+//         const b = createPersonClosureDestruct({name: 'Миколай', age: 75});
+//         console.log(b.getSurname());
+
+// }
+
+// {
+//     function isSorted (...args) {
+//         if(!args.length) {
+//             return false;
+//         }
+//         const isEveryNumber = args.every((element) => !isNaN(element));
+//         if(!isEveryNumber) {
+//             return false;
+//         }
+//         for(let i = 0; i < args.length - 1; i++){
+
+//             if((args[i] > args[i+1]))
+//                 return false;
+
+//         }
+//         return true;
+//     }
+
+//     let arr = [];
+//     let userInput;
+
+//     while(userInput = prompt('Що ви хочите поставити у вашу скриньку?')) {
+//         if(!isNaN(userInput)) {
+//             arr.push(+userInput);
+//         }
+        
+//     }
+//     console.log(isSorted(...arr));
+// }
+
+{   
     function createPersonClosure (name, surname) {
         
         let fullName = name + ' ' + surname;
@@ -79,10 +254,10 @@
                 const parts = fullName.split(' ');
                 const length = parts.length;
                 if (length >= 1) {
-                    surname = parts[length - 3] || 'Анонімович';
+                    surname = parts[length - 3] || '';
                 }
                 if (length >= 2) {
-                    name = parts.slice(0, length - 2).join(' ') || 'Анонім';
+                    name = parts.slice(0, length - 2).join(' ') || '';
                 }
                 if (length >= 3) {
                     fatherName = parts[length - 1] || '';
@@ -91,110 +266,75 @@
         }
         
     }
-    const a = createPersonClosure("Вася", "Пупкін")
-    const b = createPersonClosure("Ганна", "Іванова") 
-    console.log(a.getName())
-    a.setAge(15)
-    a.setAge(150) //не працює
-    b.setFullName("Петрова Ганна Миколаївна")
-    console.log(b.getFatherName()) //Миколаївна
-}
 
-{
-        function createPersonClosureDestruct (userInfo) {
-            
-            let {name = 'Анонім', surname = 'Анонімус', fatherName = 'Анонімович', age = 1} = userInfo;
+    function personForm(parent, person) {
+        const nameInput = document.createElement('input');
+        nameInput.setAttribute('type', 'text');
+        nameInput.value = person.getName();
+        parent.appendChild(nameInput);
     
-            return {
-                getName: function () {
-                    return name;
-                },
+        const surnameInput = document.createElement('input');
+        surnameInput.setAttribute('type', 'text');
+        surnameInput.value = person.getSurname();
+        parent.appendChild(surnameInput);
     
-                getSurname: function () {
-                    return surname;
-                },
+        const fatherNameInput = document.createElement('input');
+        fatherNameInput.setAttribute('type', 'text');
+        fatherNameInput.value = person.getFatherName();
+        parent.appendChild(fatherNameInput);
     
-                getFatherName: function () {
-                    return fatherName;
-                },
+        const ageInput = document.createElement('input');
+        ageInput.setAttribute('type', 'number');
+        ageInput.value = person.getAge();
+        parent.appendChild(ageInput);
     
-                getAge: function () {
-                    return age;
-                },
+        const fullNameInput = document.createElement('input');
+        fullNameInput.setAttribute('type', 'text');
+        fullNameInput.value = person.getFullName();
+        parent.appendChild(fullNameInput);
     
-                getFullName: function () {
-                    if (fatherName) {
-                        return `${name} ${fatherName} ${surname}`;
-                    } else {
-                        return `${name} ${surname}`;
-                    }   
-                },
+        nameInput.addEventListener('input', () => {
+            person.setName(nameInput.value);
+            nameInput.value = person.getName();
+        });
     
-                setName: function (newName) {
-                    name = newName;
-                },
+        surnameInput.addEventListener('input', () => {
+            person.setSurname(surnameInput.value);
+            surnameInput.value = person.getSurname();
+        });
     
-                setSurname: function (newSurname) {
-                    surname = newSurname;
-                },
+        fatherNameInput.addEventListener('input', () => {
+            person.setFatherName(fatherNameInput.value);
+            fatherNameInput.value = person.getFatherName();
+        });
     
-                setFatherName: function (newFatherName) {
-                    fatherName = newFatherName;
-                },
-    
-                setAge: function (newAge) {
-                    if (typeof newAge === 'number' && newAge >= 0 && newAge <= 100) {
-                        age = newAge;
-                    }
-                },
-    
-                setFullName: function(fullName) {
-                    const parts = fullName.split(' ');
-                    const length = parts.length;
-                    if (length >= 1) {
-                        surname = parts[length - 3];
-                    }
-                    if (length >= 2) {
-                        name = parts.slice(0, length - 2).join(' ');
-                    }
-                    if (length >= 3) {
-                        fatherName = parts[length - 1];
-                    }
-                }
+        ageInput.addEventListener('input', () => {
+            const age = parseInt(ageInput.value);
+            if (!isNaN(age) && age >= 0 && age <= 100) {
+                person.setAge(age);
+            } else {
+                ageInput.value = person.getAge();
             }
-            
-        }
-        const b = createPersonClosureDestruct({name: 'Миколай', age: 75});
-        console.log(b.getSurname());
-
-}
-
-{
-    function isSorted (...args) {
-        if(!args.length) {
-            return false;
-        }
-        const isEveryNumber = args.every((element) => !isNaN(element));
-        if(!isEveryNumber) {
-            return false;
-        }
-        for(let i = 0; i < args.length - 1; i++){
-
-            if((args[i] > args[i+1]))
-                return false;
-
-        }
-        return true;
+        });
+    
+        fullNameInput.addEventListener('input', () => {
+            person.setFullName(fullNameInput.value);
+            fullNameInput.value = person.getFullName();
+            const parts = fullNameInput.value.split(' ');
+            if (parts.length === 3) {
+                nameInput.value = parts[0];
+                surnameInput.value = parts[1];
+                fatherNameInput.value = parts[2];
+            }
+        });
     }
-
-    let arr = [];
-    let userInput;
-
-    while(userInput = prompt('Що ви хочите поставити у вашу скриньку?')) {
-        if(!isNaN(userInput)) {
-            arr.push(+userInput);
-        }
-        
-    }
-    console.log(isSorted(...arr));
+    
+    const parentElement = document.createElement('div');
+    document.body.appendChild(parentElement);
+    
+    const b = createPersonClosure("Ганна", "Іванова");
+    b.setAge(15);
+    b.setFullName("Петрова Ганна Миколаївна");
+    
+    personForm(parentElement, b);
 }
