6995cd8ed2cd97cffb7a5d31829774d2a562d665
maxsozo <sozanskii.maksim@gmail.com>
2024-03-20 21:51:17 +0200

    Added exercise createPersonClosureDestruct

--- Homework 10/index.js
+++ Homework 10/index.js
@@ -98,4 +98,73 @@
     a.setAge(150) //не працює
     b.setFullName("Петрова Ганна Миколаївна")
     console.log(b.getFatherName()) //Миколаївна
-    }
+}
+
+{
+        function createPersonClosureDestruct (userInfo) {
+            
+            let {name = 'Анонім', surname = 'Анонімус', fatherName = 'Анонімович', age = 1} = userInfo;
+    
+            return {
+                getName: function () {
+                    return name;
+                },
+    
+                getSurname: function () {
+                    return surname;
+                },
+    
+                getFatherName: function () {
+                    return fatherName;
+                },
+    
+                getAge: function () {
+                    return age;
+                },
+    
+                getFullName: function () {
+                    if (fatherName) {
+                        return `${name} ${fatherName} ${surname}`;
+                    } else {
+                        return `${name} ${surname}`;
+                    }   
+                },
+    
+                setName: function (newName) {
+                    name = newName;
+                },
+    
+                setSurname: function (newSurname) {
+                    surname = newSurname;
+                },
+    
+                setFatherName: function (newFatherName) {
+                    fatherName = newFatherName;
+                },
+    
+                setAge: function (newAge) {
+                    if (typeof newAge === 'number' && newAge >= 0 && newAge <= 100) {
+                        age = newAge;
+                    }
+                },
+    
+                setFullName: function(fullName) {
+                    const parts = fullName.split(' ');
+                    const length = parts.length;
+                    if (length >= 1) {
+                        surname = parts[length - 3];
+                    }
+                    if (length >= 2) {
+                        name = parts.slice(0, length - 2).join(' ');
+                    }
+                    if (length >= 3) {
+                        fatherName = parts[length - 1];
+                    }
+                }
+            }
+            
+        }
+        const b = createPersonClosureDestruct({name: 'Миколай', age: 75});
+        console.log(b.getSurname());
+
+}
