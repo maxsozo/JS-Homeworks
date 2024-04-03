b2d7a6139c3bb26a0b8d8cad3aea07273121cadb
maxsozo <sozanskii.maksim@gmail.com>
2024-04-03 16:17:17 +0300

    Delete commits

--- Homework 13/index.js
+++ Homework 13/index.js
@@ -1,161 +1,161 @@
-// {
-//     function Person(name, surname) {
+{
+    function Person(name, surname) {
 
-//     this.name = name;
-//     this.surname = surname;
-//         this.getFullName = function() {
-//                 if (this.fatherName) {
-//                     return `${this.name} ${this.fatherName} ${this.surname}`;
-//                 } else {
-//                     return `${this.name} ${this.surname}`;
-//                 }
-//         }
-//     }
+    this.name = name;
+    this.surname = surname;
+        this.getFullName = function() {
+                if (this.fatherName) {
+                    return `${this.name} ${this.fatherName} ${this.surname}`;
+                } else {
+                    return `${this.name} ${this.surname}`;
+                }
+        }
+    }
 
-//     const a = new Person("Вася", "Пупкін")
-//     const b = new Person("Ганна", "Іванова")
-//     const c = new Person("Єлизавета", "Петрова")
+    const a = new Person("Вася", "Пупкін")
+    const b = new Person("Ганна", "Іванова")
+    const c = new Person("Єлизавета", "Петрова")
 
-//     console.log(a.getFullName()) // Вася Пупкін
-//     a.fatherName = 'Іванович' 
+    console.log(a.getFullName()) // Вася Пупкін
+    a.fatherName = 'Іванович' 
 
-//     console.log(a.getFullName())// Вася Іванович Пупкін
+    console.log(a.getFullName())// Вася Іванович Пупкін
 
-//     console.log(b.getFullName()) // Ганна Іванова
-// }
+    console.log(b.getFullName()) // Ганна Іванова
+}
 
-// {
-//     function Person(name, surname) {
+{
+    function Person(name, surname) {
 
-//         this.name = name;
-//         this.surname = surname;
-//     }
+        this.name = name;
+        this.surname = surname;
+    }
 
-//     Person.prototype.getFullName = function() {
-//         if (this.fatherName) {
-//             return `${this.name} ${this.fatherName} ${this.surname}`;
-//             } else {
-//                 return `${this.name} ${this.surname}`;
-//               }
-//     }
+    Person.prototype.getFullName = function() {
+        if (this.fatherName) {
+            return `${this.name} ${this.fatherName} ${this.surname}`;
+            } else {
+                return `${this.name} ${this.surname}`;
+              }
+    }
     
-//         const a = new Person("Вася", "Пупкін")
-//         const b = new Person("Ганна", "Іванова")
-//         const c = new Person("Єлизавета", "Петрова")
+        const a = new Person("Вася", "Пупкін")
+        const b = new Person("Ганна", "Іванова")
+        const c = new Person("Єлизавета", "Петрова")
     
-//         console.log(a.getFullName()) // Вася Пупкін
-//         a.fatherName = 'Іванович' 
+        console.log(a.getFullName()) // Вася Пупкін
+        a.fatherName = 'Іванович' 
     
-//         console.log(a.getFullName())// Вася Іванович Пупкін
+        console.log(a.getFullName())// Вася Іванович Пупкін
     
-//         console.log(b.getFullName()) // Ганна Іванова
-// }
+        console.log(b.getFullName()) // Ганна Іванова
+}
 
-// {
-//     function createStore(reducer) {
-//       function Store() {
-//         let state = reducer(undefined, {});
-//         let subscribers = [];
+{
+    function createStore(reducer) {
+      function Store() {
+        let state = reducer(undefined, {});
+        let subscribers = [];
       
-//         this.getState = function() {
-//         return state;
-//         } 
-//         this.subscribe = function(cb) {
-//           subscribers.push(cb);
-//           return () => (subscribers = subscribers.filter((c) => c !== cb));
-//         };
+        this.getState = function() {
+        return state;
+        } 
+        this.subscribe = function(cb) {
+          subscribers.push(cb);
+          return () => (subscribers = subscribers.filter((c) => c !== cb));
+        };
       
-//         this.dispatch = function(action) {
-//           const newState = reducer(state, action);
-//           if (newState !== state) {
-//             state = newState;
-//             for (let cb of subscribers) cb();
-//           }
-//         };
-//       }
-//         return new Store();
-//     }
+        this.dispatch = function(action) {
+          const newState = reducer(state, action);
+          if (newState !== state) {
+            state = newState;
+            for (let cb of subscribers) cb();
+          }
+        };
+      }
+        return new Store();
+    }
       
-//       const initialState = {
-//         cash: 1000,
-//         kiosk: {
-//           beer: { quantity: 100, price: 20 },
-//           chips: { quantity: 50, price: 15 },
-//           cigarettes: { quantity: 30, price: 25 },
-//         },
-//       };
+      const initialState = {
+        cash: 1000,
+        kiosk: {
+          beer: { quantity: 100, price: 20 },
+          chips: { quantity: 50, price: 15 },
+          cigarettes: { quantity: 30, price: 25 },
+        },
+      };
       
-//       const reducer = (state = initialState, action) => {
-//         switch (action.type) {
-//           case "BUY_ITEM":
-//             const { item, quantity, cost } = action.payload;
-//             return {
-//               ...state,
-//               cash: state.cash + cost,
-//               kiosk: {
-//                 ...state.kiosk,
-//                 [item]: {
-//                   ...state.kiosk[item],
-//                   quantity: state.kiosk[item].quantity - quantity,
-//                 },
-//               },
-//             };
-//           default:
-//             return state;
-//         }
-//       };
+      const reducer = (state = initialState, action) => {
+        switch (action.type) {
+          case "BUY_ITEM":
+            const { item, quantity, cost } = action.payload;
+            return {
+              ...state,
+              cash: state.cash + cost,
+              kiosk: {
+                ...state.kiosk,
+                [item]: {
+                  ...state.kiosk[item],
+                  quantity: state.kiosk[item].quantity - quantity,
+                },
+              },
+            };
+          default:
+            return state;
+        }
+      };
       
-//       const store = createStore(reducer);
+      const store = createStore(reducer);
       
-//       const buyItem = (item, quantity, money) => {
-//         const currentState = store.getState();
-//         const selectedProduct = currentState.kiosk[item];
-//         if (selectedProduct && selectedProduct.quantity >= quantity) {
-//           const itemPrice = selectedProduct.price;
-//           const cost = itemPrice * quantity;
-//           if (cost <= money) {
-//             store.dispatch({
-//               type: "BUY_ITEM",
-//               payload: {
-//                 item,
-//                 quantity,
-//                 cost
-//               }
-//             });
-//           }
-//         } else {
-//           console.log(`Товар ${item} відсутній у баби Зіни.`);
-//         }
-//       };
+      const buyItem = (item, quantity, money) => {
+        const currentState = store.getState();
+        const selectedProduct = currentState.kiosk[item];
+        if (selectedProduct && selectedProduct.quantity >= quantity) {
+          const itemPrice = selectedProduct.price;
+          const cost = itemPrice * quantity;
+          if (cost <= money) {
+            store.dispatch({
+              type: "BUY_ITEM",
+              payload: {
+                item,
+                quantity,
+                cost
+              }
+            });
+          }
+        } else {
+          console.log(`Товар ${item} відсутній у баби Зіни.`);
+        }
+      };
       
-//       const displayKioskState = () => {
-//         const state = store.getState();
-//         kiosk.innerHTML = "";
-//         Object.entries(state.kiosk).forEach(([item, details]) => {
-//           const itemElement = document.createElement("div");
-//           itemElement.innerText = `${item}: ${details.quantity}шт. ${details.price} грн`;
-//           kiosk.appendChild(itemElement);
-//         });
+      const displayKioskState = () => {
+        const state = store.getState();
+        kiosk.innerHTML = "";
+        Object.entries(state.kiosk).forEach(([item, details]) => {
+          const itemElement = document.createElement("div");
+          itemElement.innerText = `${item}: ${details.quantity}шт. ${details.price} грн`;
+          kiosk.appendChild(itemElement);
+        });
       
-//         kioskInfo.innerText = `Кількість товару: Пивандр: ${
-//           state.kiosk.beer.quantity
-//         }, Чіпсони: ${state.kiosk.chips.quantity}, Сіги: ${
-//           state.kiosk.cigarettes.quantity
-//         }`;
+        kioskInfo.innerText = `Кількість товару: Пивандр: ${
+          state.kiosk.beer.quantity
+        }, Чіпсони: ${state.kiosk.chips.quantity}, Сіги: ${
+          state.kiosk.cigarettes.quantity
+        }`;
       
-//         document.title = `Каса: ${state.cash} грн`;
-//       };
+        document.title = `Каса: ${state.cash} грн`;
+      };
       
-//       buy.addEventListener("click", () => {
-//         const item = document.getElementById("item").value;
-//         const quantity = parseInt(document.getElementById("quantity").value);
-//         const money = parseInt(document.getElementById("money").value);
-//         buyItem(item, quantity, money);
-//       });
+      buy.addEventListener("click", () => {
+        const item = document.getElementById("item").value;
+        const quantity = parseInt(document.getElementById("quantity").value);
+        const money = parseInt(document.getElementById("money").value);
+        buyItem(item, quantity, money);
+      });
       
-//       const unsubscribe = store.subscribe(displayKioskState);
-//       displayKioskState();
-// }
+      const unsubscribe = store.subscribe(displayKioskState);
+      displayKioskState();
+}
 
 {
     function Password(parent, open) {
