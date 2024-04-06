fa4ea96a8b7a1a4d9106b74d2423729b93e60123
maxsozo <sozanskii.maksim@gmail.com>
2024-04-06 17:55:15 +0300

    Added exercise Рекурсія: DOM tree

--- Homework 14/index.js
+++ Homework 14/index.js
@@ -1,63 +1,129 @@
-const table = {
-    tagName: 'table',
-    attrs: {
-        border: "1",
-    },
-    children: [
-        {
-            tagName: 'tr',
-            children: [
-                {
-                    tagName: "td",
-                    children: ["1x1"],
-                },
-                {
-                    tagName: "td",
-                    children: ["1x2"],
-                },
-            ]
+{
+    const table = {
+        tagName: 'table',
+        attrs: {
+            border: "1",
         },
-        {
-            tagName: 'tr',
-            children: [
-                {
-                    tagName: "td",
-                    children: ["2x1"],
-                },
-                {
-                    tagName: "td",
-                    children: ["2x2"],
-                },
-            ]
-        }
-    ]
-}
-
-function htmlTree(obj) {
-    if (typeof node === 'string') {
-        return node;
+        children: [
+            {
+                tagName: 'tr',
+                children: [
+                    {
+                        tagName: "td",
+                        children: ["1x1"],
+                    },
+                    {
+                        tagName: "td",
+                        children: ["1x2"],
+                    },
+                ]
+            },
+            {
+                tagName: 'tr',
+                children: [
+                    {
+                        tagName: "td",
+                        children: ["2x1"],
+                    },
+                    {
+                        tagName: "td",
+                        children: ["2x2"],
+                    },
+                ]
+            }
+        ]
     }
 
-    const { tagName, attrs, children } = obj;
-    let str = `<${tagName}`;
-    
-    if (attrs) {
-        for (const [key, value] of Object.entries(attrs)) {
-            str += ` ${key}='${value}'`;
+    function htmlTree(obj) {
+        if (typeof obj === 'string') {
+            return obj;
         }
-    }
 
-    str += '>';
-
-    if (children) {
-        for (const child of children) {
-            str += htmlTree(child);
+        const { tagName, attrs, children } = obj;
+        let str = `<${tagName}`;
+        
+        if (attrs) {
+            for (const [key, value] of Object.entries(attrs)) {
+                str += ` ${key}='${value}'`;
+            }
         }
-    }
 
-    str += `</${tagName}>`;
-    return str;
+        str += '>';
+
+        if (children) {
+            for (const child of children) {
+                str += htmlTree(child);
+            }
+        }
+
+        str += `</${tagName}>`;
+        return str;
 }
 
 const htmlString = htmlTree(table);
 document.write(htmlString);
+}
+
+{
+    const table = {
+        tagName: 'table',
+        attrs: {
+            border: "1",
+        },
+        children: [
+            {
+                tagName: 'tr',
+                children: [
+                    {
+                        tagName: "td",
+                        children: ["1x1"],
+                    },
+                    {
+                        tagName: "td",
+                        children: ["1x2"],
+                    },
+                ]
+            },
+            {
+                tagName: 'tr',
+                children: [
+                    {
+                        tagName: "td",
+                        children: ["2x1"],
+                    },
+                    {
+                        tagName: "td",
+                        children: ["2x2"],
+                    },
+                ]
+            }
+        ]
+    }
+
+    function domTree(parent, obj) {
+        if (typeof obj === 'string') {
+            const textObj = document.createTextNode(obj);
+            parent.appendChild(textObj);
+        } else {
+            const { tagName, attrs, children } = obj;
+            const element = document.createElement(tagName);
+            
+            if (attrs) {
+                for (const [key, value] of Object.entries(attrs)) {
+                    element.setAttribute(key, value);
+                }
+            }
+            
+            parent.appendChild(element);
+            
+            if (children) {
+                for (const child of children) {
+                    domTree(element, child);
+                }
+            }
+        }
+    }
+    
+
+    domTree(document.body, table)
+}
