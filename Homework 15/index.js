c682f75fd2b8c3439e9f9860cbd0802bc1d7d140
maxsozo <sozanskii.maksim@gmail.com>
2024-04-15 15:13:38 +0300

    Removed commits

--- Homework 15/index.js
+++ Homework 15/index.js
@@ -1,135 +1,134 @@
-// {   
-//     function createTable(domElement, response) {
-//         const tag = document.createElement(domElement);
-//         tag.style.border = '1px solid black';
-//         document.body.appendChild(tag);
+{   
+    function createTable(domElement, response) {
+        const tag = document.createElement(domElement);
+        tag.style.border = '1px solid black';
+        document.body.appendChild(tag);
     
-//         const keysParameters = Object.keys(response);
+        const keysParameters = Object.keys(response);
     
-//         keysParameters.forEach((parameter) => {
-//             const paramsTh = document.createElement('th');
-//             paramsTh.style.border = '1px solid black';
-//             paramsTh.innerText = parameter;
-//             tag.appendChild(paramsTh);
-//         })
+        keysParameters.forEach((parameter) => {
+            const paramsTh = document.createElement('th');
+            paramsTh.style.border = '1px solid black';
+            paramsTh.innerText = parameter;
+            tag.appendChild(paramsTh);
+        })
     
-//         const tr = document.createElement('tr');
-//         tag.appendChild(tr);
+        const tr = document.createElement('tr');
+        tag.appendChild(tr);
     
-//         const valueParameters = Object.values(response);
+        const valueParameters = Object.values(response);
     
-//         valueParameters.forEach((parameter) => {
-//             const paramsTh = document.createElement('th');
-//             paramsTh.style.border = '1px solid black';
+        valueParameters.forEach((parameter) => {
+            const paramsTh = document.createElement('th');
+            paramsTh.style.border = '1px solid black';
             
-//             function createFetchButton(url) {
-//                 const button = document.createElement('button');
-//                 button.innerText = 'Силка';
-//                 button.addEventListener('click', () => {
-//                     fetch(url)
-//                         .then(res => res.json())
-//                         .then(data => {
+            function createFetchButton(url) {
+                const button = document.createElement('button');
+                button.innerText = 'Силка';
+                button.addEventListener('click', () => {
+                    fetch(url)
+                        .then(res => res.json())
+                        .then(data => {
 
-//                             tr.textContent = '';
-//                             createTable(domElement, data);
-//                         })
-//                 });
-//                 return button;
-//             };
+                            tr.textContent = '';
+                            createTable(domElement, data);
+                        })
+                });
+                return button;
+            };
 
-//             if (typeof parameter === 'string') {
+            if (typeof parameter === 'string') {
 
-//                 if (parameter.includes('https://swapi.dev/api/')) {
-//                     paramsTh.appendChild(createFetchButton(parameter));
-//                 } else {
-//                     paramsTh.innerText = parameter;
-//                 }
-//             } else if (Array.isArray(parameter)) {
+                if (parameter.includes('https://swapi.dev/api/')) {
+                    paramsTh.appendChild(createFetchButton(parameter));
+                } else {
+                    paramsTh.innerText = parameter;
+                }
+            } else if (Array.isArray(parameter)) {
 
-//                 parameter.forEach(item => {
-//                     const itemContainer = document.createElement('th');
-//                     if (typeof item === 'string') {
+                parameter.forEach(item => {
+                    const itemContainer = document.createElement('th');
+                    if (typeof item === 'string') {
 
-//                         if (item.includes('https://swapi.dev/api/')) {
-//                             itemContainer.appendChild(createFetchButton(item));
-//                         } else {
+                        if (item.includes('https://swapi.dev/api/')) {
+                            itemContainer.appendChild(createFetchButton(item));
+                        } else {
 
-//                             itemContainer.innerText = item;
-//                         }
-//                     }
-//                     paramsTh.appendChild(itemContainer);
-//                 });
-//             }
+                            itemContainer.innerText = item;
+                        }
+                    }
+                    paramsTh.appendChild(itemContainer);
+                });
+            }
     
-//             tr.appendChild(paramsTh);
-//         });
-//     }
+            tr.appendChild(paramsTh);
+        });
+    }
     
-//     fetch('https://swapi.dev/api/people/1/')
-//     .then(res => res.json())
-//     .then(luke => { 
-//         createTable('table', luke);
-//     });
-// }
+    fetch('https://swapi.dev/api/people/1/')
+    .then(res => res.json())
+    .then(luke => { 
+        createTable('table', luke);
+    });
+}
 
-// {
-//     function delay(ms) {
-//         return new Promise((fulfill, reject) => {
-//             setTimeout(() => fulfill(`Delay for delay: ${ms} ms`), ms);
-//         });
-//     }
+{
+    function delay(ms) {
+        return new Promise((fulfill, reject) => {
+            setTimeout(() => fulfill(`Delay for delay: ${ms} ms`), ms);
+        });
+    }
     
-//     function fetchFromAPI() {
-//         return fetch('https://swapi.dev/api/people/1/')
-//             .then(response => response.json())
-//             .then(data => `Data from API: ${data.name}`);
-//     };
+    function fetchFromAPI() {
+        return fetch('https://swapi.dev/api/people/1/')
+            .then(response => response.json())
+            .then(data => `Data from API: ${data.name}`);
+    };
     
-//     Promise.race([delay(120), fetchFromAPI()])
-//         .then(result => console.log(result))
-//         .catch(error => console.error(error , 'Щось сє стресло'))
-// }
+    Promise.race([delay(120), fetchFromAPI()])
+        .then(result => console.log(result))
+        .catch(error => console.error(error , 'Щось сє стресло'))
+}
 
-// {
-//     function confirmPromise(text) {
-//         return new Promise((resolve , reject) => {
-//             const result = confirm(text);
-//             if(result) {
-//                 resolve('OK')
-//             }
-//             else {
-//                 reject('Cancel')
-//             }
-//         });
-//     }
+{
+    function confirmPromise(text) {
+        return new Promise((resolve , reject) => {
+            const result = confirm(text);
+            if(result) {
+                resolve('OK')
+            }
+            else {
+                reject('Cancel')
+            }
+        });
+    }
     
-//     confirmPromise('Проміси це складно?').then(() => console.log('не так вже й складно'),
-//                                             () => console.log('respect за посидючість і уважність'))
-// }
+    confirmPromise('Проміси це складно?').then(() => console.log('не так вже й складно'),
+                                            () => console.log('respect за посидючість і уважність'))
+}
 
-// {
-//     function promptPromise(text) {
-//         return new Promise((resolve , reject) => {
-//             const name = prompt(text);
-//             if(name) {
-//                 resolve(name);
-//             }
-//             else {
-//                 reject('Cancel');
-//             }
-//         });
-//     }
+{
+    function promptPromise(text) {
+        return new Promise((resolve , reject) => {
+            const name = prompt(text);
+            if(name) {
+                resolve(name);
+            }
+            else {
+                reject('Cancel');
+            }
+        });
+    }
     
-//     promptPromise("Як тебе звуть?").then(name => console.log(`Тебе звуть ${name}`),
-//                                        () => console.log('Ну навіщо морозитися, нормально ж спілкувалися'))
-// }
+    promptPromise("Як тебе звуть?").then(name => console.log(`Тебе звуть ${name}`),
+                                       () => console.log('Ну навіщо морозитися, нормально ж спілкувалися'))
+}
 
 {
     function LoginForm(parent) {
         const form = document.createElement('form');
         parent.appendChild(form);
     
-        // Оголошуємо конструктор Password як член LoginForm
         function Password(open) {
             const input = document.createElement('input');
             input.type = open ? 'text' : 'password';
